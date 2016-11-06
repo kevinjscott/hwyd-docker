a957336c85e58d49cc9894aec1b8e0f5a2ffc37f
Kevin J. Scott <kevinjscott@gmail.com>
2016-11-06 18:57:42 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # hwyd-docker
+
+`docker build -t kevinjscott/hwyd-docker .
+
+`docker push kevinjscott/hwyd-docker
+
+`docker run -p 3000:3000 kevinjscott/hwyd-docker
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express! - via local Build' });
+  res.render('index', { title: 'Express! - via (actual) 3 local Build' });
 });
 
 module.exports = router;
