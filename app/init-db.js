2cbe7e43f57deef6419a722ed8788ced022ab0c1
Kevin J. Scott <kevinjscott@gmail.com>
2016-11-15 00:12:26 -0500

    cleanup

--- app/init-db.js
+++ app/init-db.js
@@ -1,18 +1,16 @@
 var teachers = require('./data-teachers');
 var customers = require('./data-customers');
-var _ = require('lodash');
-var moment = require('moment-timezone');
 var Promise = require('bluebird');
 var mongoose = Promise.promisifyAll(require("mongoose"));
 var db = mongoose.connection;
-var messages = Promise.promisifyAll(require('./messages'));
-mongoose.connect(process.env.DATABASE_URL);
-db.on('error', console.error.bind(console, 'connection error:'));
 
 exports.init = function(callback){
-  require('./messages').initAsync()
-  .then(function(){
-    if (callback) callback(null);
-  });
+  mongoose.connect(process.env.DATABASE_URL);
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    Promise.promisifyAll(require('./messages')).initAsync()
+    .then(function(){
+      if (callback) callback(null);
+    });
+  })
 }
-
